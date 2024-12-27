5e81b3cb9747dd198bc86b8c928c788b511ad020
koukanni <willyhuang20050000+restart@gmail.com>
2024-12-27 20:07:58 +0800

    Add psql initialization file and update docker-compose.yml
    - Added psql initialization file.
    - Updated docker-compose.yml to include the new psql initialization file and the sleep_health_lifestyle_dataset.csv file in the volumes section.

---
+++ docker/import_data.sql
@@ -0,0 +1,1 @@
+\COPY sleep_health_lifestyle_dataset FROM '/docker-entrypoint-initdb.d/sleep_health_lifestyle_dataset.csv' DELIMITER ',' CSV HEADER;
---
+++ docker/init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS sleep_health_lifestyle_dataset (
+    "Person ID" SERIAL PRIMARY KEY,
+    "Gender" TEXT NOT NULL,
+    "Age" INTEGER NOT NULL,
+    "Occupation" TEXT NOT NULL,
+    "Sleep Duration (hours)" DOUBLE PRECISION NOT NULL,
+    "Quality of Sleep (scale: 1-10)" DOUBLE PRECISION NOT NULL,
+    "Physical Activity Level (minutes/day)" INTEGER NOT NULL,
+    "Stress Level (scale: 1-10)" INTEGER NOT NULL,
+    "BMI Category" TEXT NOT NULL,
+    "Blood Pressure (systolic/diastolic)" TEXT NOT NULL,
+    "Heart Rate (bpm)" INTEGER NOT NULL,
+    "Daily Steps" INTEGER NOT NULL,
+    "Sleep Disorder" TEXT NOT NULL
+);
