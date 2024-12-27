0e22983b91cd5df5db8eff72e388040fa005b4ff
koukanni <willyhuang20050000+restart@gmail.com>
2024-12-27 21:31:56 +0800

    Add error handling for Prisma database connection in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client';
+import { config } from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const Prisma = new PrismaClient();
+  try {
+    await Prisma.$connect();
+    console.log('Prisma Connected to the database');
+  } catch (error) {
+    console.error('Prisma failed to connect to the database');
+  }
   await app.listen(process.env.PORT ?? 3000);
 }
+config();
 bootstrap();
